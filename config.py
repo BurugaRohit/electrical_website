f594f3752f46984a0e4b8f81b41ab33cde96b087
BurugaRohit <31987498+BurugaRohit@users.noreply.github.com>
2018-12-09 00:05:27 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,22 @@
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+class Config(object):
+    DEBUG = False
+    TESTING = False
+    CSRF_ENABLED = True
+    SECRET_KEY = 'some_secret'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'pymysql:///.electrical_site.db')
+
+class ProductionConfig(Config):
+    DEBUG =False
+
+class StagingConfig(Config):
+    DEVELOPMENT = True
+    DEBUG = True
+
+class DevelopmentConfig(Config):
+    DEVELOPMENT = True
+    DEBUG = True
+class TestingConfig(Config):
+    TESTING = True
